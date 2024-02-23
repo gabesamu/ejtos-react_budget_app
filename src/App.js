69f99efd338272d25cd011be17eb8fc88ab5f353
Gabriel <gmsamuel18@gmail.com>
2024-02-22 19:04:48 -0600

    add currency component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
 import AllocationForm from './components/AllocationForm';
+import Currency from './components/currency';
 
 
 import { AppProvider } from './context/AppContext';
@@ -18,45 +18,28 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
-                                            // Budget component
+                        <div className='col-sm'>
+                            <Currency />
+                        </div>
                         <div className='col-sm'>
                             <Budget />
                         </div>
-                        }
 
-                        {
                         <div className='col-sm'>
                             <Remaining />
                         </div>
-                        }
 
-                        {
                         <div className='col-sm'>
                             <ExpenseTotal />
                         </div>
-                        }
 
-                        {
-                            /* Add ExpenseList component here */
-                            <div className='col-sm'>
-                                <ExpenseList />
-                            </div>
-                        }
+                        <div className='col-sm'>
+                            <ExpenseList />
+                        </div>
 
-                        {
-                            /* Add ExpenseItem component here */
-                            <div className='col-sm'>
-                                <ExpenseItem />
-                            </div>
-                        }
-
-                        {
-                            /* Add AllocationForm component here under */
-                            <div className='col-sm'>
-                                <AllocationForm />
-                            </div>
-                        }
+                        <div className='col-sm'>
+                            <AllocationForm />
+                        </div>
 
                 </div>
             </div>
